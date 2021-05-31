e7096608cb5f79df1143fef19901fdfe0ff8186f
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-05-31 18:57:10 -0500

    Roulette method for selection

--- principal.py
+++ principal.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
+import numpy as np
 import random
 import math
 
 root = Tk()
-individues = []
 lSelection = []
 countPob = 0
+countGen = 0
 grav = 9.81
 
 fields = (
@@ -31,16 +32,60 @@ def calculateFitness(Xmax, Xobj):
     fitness = abs(Xobj - Xmax)
     return fitness
 
+def getProbAcu(limit):
+    global lSelection
+    a = 0
+    for i in range(0, limit, 1):
+        a += lSelection[i]['Prob']
+    return a
+
 def evaluation(inp):
     global lSelection
     global countPob
-    global individues
+    global countGen
     totFitness = 0
     promFitness = 0
-    for i in range(len(individues)):
-        individues[i]['Xmax'] = calculateXMax(individues[i]['Vo'], individues[i]['Ele'])
-        individues[i]['Fitness'] = calculateFitness(individues[i]['Xmax'], float(inp['Posición objetivo X'].get()))
-    printList(individues)
+    for i in range(len(lSelection)):
+        lSelection[i]['Xmax'] = calculateXMax(lSelection[i]['Vo'], lSelection[i]['Ele'])
+        lSelection[i]['Fitness'] = calculateFitness(lSelection[i]['Xmax'], float(inp['Posición objetivo X'].get()))
+    for i in range(len(lSelection)):
+        totFitness += lSelection[i]['Fitness']
+    for i in range(len(lSelection)):
+        lSelection[i]['Prob'] = lSelection[i]['Fitness'] / totFitness
+    auxPob = int(inp['Población máxima'].get()) - int(inp['Población inicial'].get())
+    randNumbers = np.random.rand(auxPob)
+    for i in range(len(randNumbers)):
+        aux = []
+        for j in range(len(lSelection)):
+            if j == 0:
+                aux = [0, float(lSelection[j]['Prob'])]
+            else:
+                prob = getProbAcu(j)
+                aux = [float(prob), float((prob + lSelection[j]['Prob']))]
+            if randNumbers[i] >= aux[0] and randNumbers[i] <= aux[1] and countPob <= int(inp['Población máxima'].get()):
+                lSelection[j]['Count'] += 1
+                countPob += 1
+                # print('Se encontro',randNumbers[i], ' en aux:',aux,'\nPoblación act: ',countPob)
+                break
+    printList(lSelection)
+    for i in range(len(lSelection)):
+        if i == 0:
+            maxFitness = lSelection[0]['Fitness']
+            VoMax = lSelection[0]['Vo']
+            EleMax = lSelection[0]['Ele']
+            minFitness = lSelection[0]['Fitness']
+        else:
+            # Minimizando
+            # Invertir operadores en if's si desea maximizar
+            if maxFitness > lSelection[i]['Fitness']:
+                maxFitness = lSelection[i]['Fitness']
+                VoMax = lSelection[i]['Vo']
+                EleMax = lSelection[i]['Ele']
+            if minFitness < lSelection[i]['Fitness']:
+                minFitness = lSelection[i]['Fitness']
+    print('Sum fitness: ',totFitness)
+    print('Prom fitness: ',(totFitness/len(lSelection)))
+    print('Generation: ', countGen+1,' Vo: ', VoMax,' Ele:', EleMax, ' maxFitness: ', maxFitness, ' minFitness: ', minFitness)
 
 def createIndividues(pobIni):
     global countPob
@@ -52,8 +97,8 @@ def createIndividues(pobIni):
     return aux
 
 def initialize(inp):
-    global individues
-    individues = createIndividues(int(inp['Población inicial'].get()))
+    global lSelection
+    lSelection = createIndividues(int(inp['Población inicial'].get()))
 
 def start(input):
     initialize(input)
